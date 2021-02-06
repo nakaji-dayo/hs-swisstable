b5003d8e2229bca9a48d2e386ca6e7f15ffcfc6a
Daishi Nakajima <nakaji.dayo@gmail.com>
2021-02-06 19:29:30 +0900

    iterate with bitmask

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,7 +21,9 @@ import           Data.Hashable
 -- import qualified Data.Vector           as V
 -- import           Data.Vector.Mutable   (MVector)
 -- import qualified Data.Vector.Mutable   as V
+import           Control.Exception        (assert)
 import           Control.Monad.Primitive  (PrimMonad (PrimState), RealWorld)
+import           Data.Maybe               (listToMaybe, mapMaybe)
 import           Data.Primitive
 import           Data.Primitive.Array     as A
 import           Data.Primitive.PrimArray
@@ -57,35 +59,59 @@ newSized n = do
   es <- A.newArray n Nothing
   c <- newPinnedPrimArray (n + 32)
   setPrimArray c 0 n 128
+  setPrimArray c n 32 127
   pure $ Table es c (fromIntegral n)
 
 insert' :: (PrimMonad m, Hashable k) => (k -> Int) -> k -> Table (PrimState m) k -> m ()
 insert' hash' k m = do
   let h1' = h1 hash' k
-  let idx = (size m - 1) .&. h1'
-  let pc = PP.advancePtr (mutablePrimArrayContents (ctrl m)) idx
-  let mask = cLoadMovemask pc
-  let idx' = idx + cFfs mask - 1
-  writeArray (elems m) idx' (Just k)
-  writePrimArray (ctrl m) idx' (h2 hash' k)
+  findAndWrite ((size m - 1) .&. h1') False
+  where
+    findAndWrite idx s = do -- todo rm s
+      let pc = PP.advancePtr (mutablePrimArrayContents (ctrl m)) idx
+      let mask = cLoadMovemask pc
+      let offset = cFfs mask - 1
+      if offset < 0 then assert (not s) $ findAndWrite 0 True else (do
+        let idx' = idx + offset
+        writeArray (elems m) idx' (Just k)
+        writePrimArray (ctrl m) idx' (h2 hash' k))
 
-insert :: (PrimMonad m, Hashable k) => k -> Table (PrimState m) k -> m ()
-insert = insert' hash
-
-lookup :: (PrimMonad m, Hashable k)
-  => k -> Table (PrimState m) k -> m (Maybe k)
-lookup = lookup' hash
-
-lookup' :: (PrimMonad m, Hashable k)
+lookup' :: (PrimMonad m, Hashable k, Show k, Eq k)
   => (k -> Int) -> k -> Table (PrimState m) k -> m (Maybe k)
 lookup' hash' k m = do
   let h1' = h1 hash' k
       h2' = h2 hash' k
   let idx = (size m - 1) .&. h1'
-  let pc = PP.advancePtr (mutablePrimArrayContents (ctrl m)) idx
-  let mask = cElmCmpVec h2' pc
-  let idx' = idx + cFfs mask - 1
-  readArray (elems m) idx'
+  let find idx s = do
+        let pc = PP.advancePtr (mutablePrimArrayContents (ctrl m)) idx
+        let mask = cElmCmpVec h2' pc
+        let bitidxs = map cFfs $ iterate (\x -> x .&. x-1) mask
+        -- recursive with bitmask
+        let f (bidx:bidxs)
+              | bidx == 0 = pure Nothing
+              | otherwise = do
+                  let idx' = idx + bidx - 1
+                  e <- readArray (elems m) idx'
+                  case e of
+                    Just k'
+                      | k == k' -> pure e -- todo: iter bit
+                      | otherwise -> f bidxs
+                    _       -> f bidxs
+        f bitidxs >>= \case
+          Nothing
+            | cElmCmpVec 128 pc /= 0 -> pure Nothing -- found empty
+            | otherwise -> do
+                let n = idx + 32
+                if n > size m then assert (not s) (find 0 True) else find n s
+          x       -> pure x
+        -- let next = do
+        --       let n = idx' + 32
+        --       traceShowM ("e", k, e)
+        --       if n > size m then assert (not s) (find 0 True) else find n s
+  find idx False
+
+-- firstJust :: (a -> Maybe b) -> [a] -> Maybe b
+-- firstJust f = listToMaybe . mapMaybe f
 
 h1 :: Hashable k => (k -> Int) -> k -> Int
 h1 = ($)
@@ -94,3 +120,10 @@ h2 :: Hashable a => (a -> Int) -> a -> Word8
 h2 h x = fromIntegral $ h x .&. 127
 
 delete = undefined
+
+insert :: (PrimMonad m, Hashable k) => k -> Table (PrimState m) k -> m ()
+insert = insert' hash
+
+lookup :: (PrimMonad m, Hashable k, Show k, Eq k)
+  => k -> Table (PrimState m) k -> m (Maybe k)
+lookup = lookup' hash
--- test/Test/Basic.hs
+++ test/Test/Basic.hs
@@ -16,14 +16,27 @@ unit_insertAndLookup = do
   let ks = ["A","Z", "C", "Y", "E", "X", "G", "W"]
   t <- new
   mapM_ (`insert` t) ks
-  ia <- freezeArray  (elems t) 0 (sizeofMutableArray $ elems t)
-  print ("test1", ia)
+  -- ia <- freezeArray  (elems t) 0 (sizeofMutableArray $ elems t)
+  -- print ("test1", ia)
   -- ictrl <- freezePrimArray (ctrl t) 0 initialCap
   -- print ictrl
   -- print  "-----------"
   forM_ ks $ \k -> do
     h <- lookup k t
     Just k @=? h
+
+unit_insert_conflict :: IO ()
+unit_insert_conflict = do
+  let ks = ["head", "Z", "C", "last"]
+  t <- newSized 8
+  mapM_ (\x -> insert' h x t) ks
+  ia <- freezeArray  (elems t) 0 (sizeofMutableArray $ elems t)
+  print ia
+  forM_ ks $ \k -> do
+    h <- lookup' h k t
+    Just k @=? h
+ where
+   h = const 0
 
 unit_insert_right_overflow :: IO ()
 unit_insert_right_overflow = do
@@ -35,6 +48,16 @@ unit_insert_right_overflow = do
   forM_ ks $ \k -> do
     h <- lookup' h k t
     Just k @=? h
+ where
+   h = const 7
+
+unit_lookup_nothing :: IO ()
+unit_lookup_nothing = do
+  let ks = ["A", "B", "C", "D"]
+  t <- new
+  mapM_ (\x -> insert' h x t) ks
+  h <- lookup' h "X" t
+  h @=? Nothing
  where
    h = const 7
 
