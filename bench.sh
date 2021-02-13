eaf9154755c47b1c9bba300b30ae44f26b1cc8e2
Daishi Nakajima <nakaji.dayo@gmail.com>
2021-02-14 02:35:54 +0900

    build settings

--- bench.sh
+++ bench.sh
@@ -4,4 +4,4 @@ set -eu
 
 mkdir -p tmp
 name=${1-}
-stack install --ghc-options="-O2" --ghc-options="-optc -mavx2 -O2" && swisstable-bench -o tmp/bench.html -m pattern $name && cp tmp/bench.html tmp/bench-`date "+%Y%m%d_%H%M%S"`.html
+stack install && swisstable-bench -o tmp/bench.html -m pattern $name && cp tmp/bench.html tmp/bench-`date "+%Y%m%d_%H%M%S"`.html
